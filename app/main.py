ee844f68303413da46ad3905e1f1ac8653dd43e2
Ruslan Naumov <rusnaumoff@gmail.com>
2023-04-29 12:30:12 +0300

    Added counter requests, shutdown and start apps

--- app/main.py
+++ app/main.py
@@ -1,27 +1,41 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, g
 import socket
 import asyncio
 from random import randint
 
 app = Flask(__name__)
 
-check_cnt = 0
+app.config['requests_counter'] = 0
 
 
 @app.route('/check')
 def check():
-    global check_cnt
-    check_cnt += 1
     return make_response('/', 200)
 
 
 @app.route('/')
 async def index():
-    global check_cnt
-    print('i am alive')
     await asyncio.sleep(randint(3, 4))
-    return f"Ready task from {socket.gethostname()} {check_cnt=}"
+    return f"Ready task from {socket.gethostname()}"
+
+
+@app.before_request()
+def before_request():
+    g.requests_counter = app.config['requests_counter']
+    app.config['requests_counter'] += 1
+
+
+@app.after_request()
+def after_request(response):
+    g.requests_counter = app.config['requests_counter']
+    app.config['requests_counter'] -= 1
+    return response
+
+
+@app.route('/requests_count')
+async def cnt_requests():
+    return str(g.requests_counter)
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="127.0.0.1", port=5000)
--- balancer/main.py
+++ balancer/main.py
@@ -3,17 +3,21 @@ from flask import Flask, Response
 import requests
 import itertools
 import aiohttp
+import socket
+import docker
 
 app = Flask(__name__)
 
+client = docker.from_env()
 UPDATE_PERIOD = 5
 
-# список серверов
+# Cписок серверов
 server_list = [
-    {"host": "loadbalancerqwe_app_1", "port": 5000, 'is_alive': True},
-    {"host": "loadbalancerqwe_app_2", "port": 5000, 'is_alive': True},
+    {"host": "loadbalancerqwe_app_1", "port": 5000, 'is_alive': True, 'requests_count': 0},
+    {"host": "loadbalancerqwe_app_2", "port": 5000, 'is_alive': True, 'requests_count': 0},
 ]
-# servers = filter(lambda s: s['is_alive'], itertools.cycle(server_list))
+
+
 servers = itertools.cycle(server_list)
 
 
@@ -26,9 +30,8 @@ def proxy():
     response = Response(resp.content, resp.status_code, headers)
     return response
 
-
+# Проверка инстансов
 async def update_statuses():
-
     check_urls = [
         f"http://{serv['host']}:{serv['port']}/check"
         for serv in server_list
@@ -39,8 +42,33 @@ async def update_statuses():
             serv['is_alive'] = status
 
 
-async def fetch_all(session, urls):
+async def get_counter_requests():
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for server in server_list:
+            url = f"http://{server['host']}:{server['port']}/requests_count"
+            task = asyncio.create_task(session.get(url))
+            tasks.append(task)
 
+        responses = await asyncio.gather(*tasks)
+        servers_requests_count = []
+
+        for server, response in zip(server_list, responses):
+            server_data = {
+                'host': server['host'],
+                'port': server['port'],
+                'requests_count': int(await response.text())
+            }
+            servers_requests_count.append(server_data)
+
+        return servers_requests_count
+
+
+
+
+
+
+async def fetch_all(session, urls):
     tasks = []
     for url in urls:
         task = asyncio.create_task(session.get(url))
@@ -50,16 +78,54 @@ async def fetch_all(session, urls):
 
 
 async def status_updater():
-
     while True:
         await update_statuses()
         await asyncio.sleep(UPDATE_PERIOD)
 
 
 
+# Включение и отключение инстансов
+
+async def shutdown_server(server):
+    container = client.containers.get(server)
+    container.stop()
+
+
+async def start_server(server):
+    container = client.containers.get(server)
+    container.start()
+
+
+async def check_servers(server_list):
+    server1 = server_list[0]
+    server2 = server_list[1]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result1 = sock.connect_ex((server1['host'], server1['port']))
+    result2 = sock.connect_ex((server2['host'], server2['port']))
+    if result1 != 0 and result2 != 0:
+        sock.close()
+        return True
+
+
+async def start_close_instances(server_list):
+    while True:
+        server1 = f"{server_list[0]['host']}"
+        server2 = f"{server_list[1]['host']}"
+        host = '5000'
+
+        await asyncio.sleep(120)
+        await shutdown_server(server1)
+        await asyncio.sleep(120)
+        await shutdown_server(server2)
+
+        if check_servers(server_list):
+            await start_server(server1)
+            await start_server(server2)
+
+
 if __name__ == '__main__':
+    # healthcheck
     asyncio.run(status_updater())
-    app.run(host="0.0.0.0", port=5000)
+    asyncio.run(start_close_instances(server_list))
 
-
-
+    app.run(host="127.0.0.1", port=5000)
